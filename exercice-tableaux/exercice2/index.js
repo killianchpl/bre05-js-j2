af788643690e0a2f267635ae5959e6db53c0676f
Killian Chapelain <killian.chapelain@outlook.com>
2026-01-07 09:06:25 +0000

    exercice 2 save

---
+++ exercice-tableaux/exercice2/index.js
@@ -0,0 +1,16 @@
+let animals_even = ["Dog", "Cat", "Spider"];
+
+for (let i = 0; i < animals_even.length; i++) {
+    if (i % 2 === 0) {
+
+        console.log(animals_even[i])
+    }
+}
+
+let animals_odd = ["Bat", "Snake", "Bear"];
+
+for (let i = 0; i < animals_even.length; i++) {
+    if (i % 2 === 1) {
+        console.log(animals_odd[i])
+    }
+}
