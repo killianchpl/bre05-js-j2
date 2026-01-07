fcbe0af6139d4fb16c3012819c6fa30ebc74dc99
Killian Chapelain <killian.chapelain@outlook.com>
2026-01-07 16:24:14 +0000

    exercice fonction 1

---
+++ exercice-fonctions/exercice1/index.js
@@ -0,0 +1,41 @@
+// Pour tester
+
+function add(number1, number2) {
+    let result = number1 + number2;
+
+    return result;
+}
+
+function sub(number1, number2) {
+    let result = number1 - number2;
+
+    return result;
+}
+
+
+function mult(number1, number2) {
+    let result = number1 * number2;
+
+    return result;
+}
+
+function div(number1, number2) {
+    let result = number1 / number2;
+
+    return result;
+}
+
+function mod(number1, number2) {
+    let result = number1 % number2;
+
+    return result;
+}
+
+
+
+
+console.log(add(21, 21)); // doit afficher 42
+console.log(sub(63, 21)); // doit afficher 42
+console.log(mult(21, 2)); // doit afficher 42
+console.log(div(84, 2)); // doit afficher 42
+console.log(mod(42, 2)); // doit afficher 0 a
